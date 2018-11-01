71fb3f661edf6653ac864cd574f144218fc2a8be
putuayu202 <dwikydanov123@gmail.com>
2018-11-01 09:31:53 +0700

    add featured component

--- input.php
+++ input.php
@@ -1,50 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Membuat CRUD Dengan PHP Dan MySQL - Menampilkan data dari database</title>
+	<title>Menginput Data</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<div class="judul">
-		<h1>Membuat CRUD Dengan PHP Dan MySQL</h1>
-		<h2>Menampilkan data dari database</h2>
-
-	</div>
-
+	<div class="judul" align="center">
+		<h1>Menginput data ke php-api</h1>
 	<br/>
-
-	<a href="index.php">Lihat Semua Data</a>
-
 	<br/>
 	<h3>Input data baru</h3>
 	<form action="input-api.php" method="post">
 		<table>
-      <tr>
-        <td>Id</td>
-        <td><input type="text" name="id"></td>
-       </tr>
+      		<tr>
+      		  <td>Id</td>
+      		  <td><input type="text" name="id" class="isi"></td>
+      		</tr>
 			<tr>
 				<td>Username</td>
-				<td><input type="text" name="username"></td>
+				<td><input type="text" name="username" class="isi"></td>
 			</tr>
 			<tr>
 				<td>Password</td>
-				<td><input type="text" name="password"></td>
+				<td><input type="text" name="password" class="isi"></td>
 			</tr>
 			<tr>
 				<td>Level</td>
-				<td><input type="text" name="level"></td>
+				<td><input type="text" name="level" class="isi"></td>
 			</tr>
       		<tr>
 				<td>Fullname</td>
-				<td><input type="text" name="fullname"></td>
+				<td><input type="text" name="fullname" class="isi"></td>
 			</tr>
 			<tr>
-				<td></td>
-				<td><input type="submit" value="Simpan"></td>
+				<td><a href="index.php">Lihat Semua Data</a></td>
+				<td align="right"><input type="submit" value="Simpan"></td>
 			</tr>
 		</table>
 	</form>
+</div>
 </body>
 </html>
  
